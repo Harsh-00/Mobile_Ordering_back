a6b5002d22796de840596b8bbd418a5d6a418a93
Harsh Agarwal <harshulkh@gmail.com>
2023-12-27 13:32:19 +0530

    done

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ require("dotenv").config();
 app.use(cors());
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // for urlencoded data like form data
 
 const Route = require("./routes/Route");
 app.use("/mobiles", Route);
