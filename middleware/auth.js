d3bd1abd99d56ace3cb517806c50968c83c38207
Harsh Agarwal <harshulkh@gmail.com>
2024-01-27 12:05:44 +0530

    auth implemented

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,8 +1,11 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-async function authentication(req, res, next) {
-	const token = req.header["Authorization"].replace("Bearer", "");
+//verifies token passed in header
+function authentication(req, res, next) {
+	console.log(req.headers);
+	const token = req.headers["authorization"]?.replace("Bearer", "")?.trim();
+	console.log(token);
 	if (!token) {
 		return res.status(404).json({
 			success: false,
@@ -10,20 +13,62 @@ async function authentication(req, res, next) {
 		});
 	}
 
-	//verifying token
-	jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decode) => {
-		if (err) {
-			return res.status(401).json({
-				success: false,
-				message: "Invalid Token",
-			});
-		}
-
-		console.log(decode);
+	try {
+		const decode = jwt.verify(token, process.env.JWT_SECRET_KEY);
+		console.log("YESSS-> ", decode);
 		req.user = decode;
-	});
-
+	} catch (error) {
+		return res.status(401).json({
+			success: false,
+			message: "Invalid Token",
+		});
+	}
 	next();
 }
 
-module.exports = authentication;
+function isSeller(req, res, next) {
+	if (req.user.role !== "Seller") {
+		return res.status(401).json({
+			success: false,
+			message: "Unauthorized Access",
+		});
+	}
+	next();
+}
+
+function isAdmin(req, res, next) {
+	if (req.user.role !== "Admin") {
+		return res.status(401).json({
+			success: false,
+			message: "Unauthorized Access",
+		});
+	}
+	next();
+}
+
+function isCustomer(req, res, next) {
+	if (req.user.role !== "Customer") {
+		return res.status(401).json({
+			success: false,
+			message: "Unauthorized Access",
+		});
+	}
+	next();
+}
+function isNotCustomer(req, res, next) {
+	if (req.user.role == "Customer") {
+		return res.status(401).json({
+			success: false,
+			message: "Unauthorized Access",
+		});
+	}
+	next();
+}
+
+module.exports = {
+	authentication,
+	isSeller,
+	isAdmin,
+	isCustomer,
+	isNotCustomer,
+};
