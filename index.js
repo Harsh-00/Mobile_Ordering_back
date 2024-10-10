123551ae0dbcd1f790d1a7f1e34718874598ee2b
Harsh Agarwal <harshulkh@gmail.com>
2024-10-10 22:21:11 +0530

    fix

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port=process.env.PORT || 5000;
 
 // app.use(cors());
 app.use(cors({
-	origin: 'https://mobile-ordering-front.vercel.app',
+	origin: process.env.FRONT_URL,
 	methods: ['GET', 'POST', 'PUT', 'DELETE'], 
 	credentials: true,
   }));
